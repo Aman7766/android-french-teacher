33c1873cc70909f8fa6493e51e6cbb1113586646
Amandeep <amankumar283@gmail.com>
2023-07-03 00:33:35 -0400

    id Modified

--- app/src/main/java/com/amandeep/frenchteacher/MainActivity.java
+++ app/src/main/java/com/amandeep/frenchteacher/MainActivity.java
@@ -2,12 +2,22 @@ package com.amandeep.frenchteacher;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    Button yellow,red,green,purple,blue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
