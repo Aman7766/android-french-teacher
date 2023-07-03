337ce29a203ed574c6fc10ebfbf64f0b3c53758e
Amandeep <amankumar283@gmail.com>
2023-07-03 01:30:25 -0400

    Completed

--- app/src/main/java/com/amandeep/frenchteacher/MainActivity.java
+++ app/src/main/java/com/amandeep/frenchteacher/MainActivity.java
@@ -1,6 +1,8 @@
 package com.amandeep.frenchteacher;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +16,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+            yellow=findViewById(R.id.yellow);
+            red=findViewById(R.id.red);
+            green=findViewById(R.id.green);
+            blue=findViewById(R.id.blue);
+            purple=findViewById(R.id.purple);
+        yellow.setOnClickListener(this);
+        red.setOnClickListener(this);
+        green.setOnClickListener(this);
+        blue.setOnClickListener(this);
+        purple.setOnClickListener(this);
 
-    @Override
-    public void onClick(View v) {
+
+        }
+
+        @Override
+        public void onClick(View v) {
+        if(v==yellow)
+        {
+       SayColor(v,"yellow");
+        }
+        else if(v==red)
+        {
+            SayColor(v,"red");
+        }
+        else if(v==green)
+        {
+            SayColor(v,"green");
+        }
+        else if(v==blue)
+        {  SayColor(v,"blue");
+
+        }
+        else if(v==purple)
+        {
+            SayColor(v,"purple");
+        }
+        }
+
+    public void SayColor(View v, String value)
+    {
+        Button btn= (Button)v;
+        MediaPlayer mediaPlayer=MediaPlayer.create(getApplicationContext(),getResources().getIdentifier(value,"raw",getPackageName()));
+         mediaPlayer.start();
 
     }
 }
